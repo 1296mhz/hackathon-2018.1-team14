1b2d3f358fdd7e767554a0452f635dc0bd1ba88d
vugluskr86 <ivashkin.vasiliy@gmail.com>
2018-04-08 05:59:18 +0500

    mob tpl

---
+++ src/objects/Mob.js
@@ -0,0 +1,19 @@
+export default class Mob extends Phaser.Sprite {
+    constructor({game, x, y, key, frame}) {
+        super(game, x, y, key, frame);
+
+        this.game.add.existing(this);
+        this.anchor.setTo(0.5);
+
+        this.game.physics.enable(this, Phaser.Physics.ARCADE);
+        
+        this.body.immovable = false;
+        this.body.collideWorldBounds = true;
+        this.body.bounce.setTo(1, 1);
+        this.body.collideWorldBounds = true;
+    }
+
+    update() {
+
+    }
+}
